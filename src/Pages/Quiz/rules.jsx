667442f539a7622b9b29a7f7eeae466ee6b2d5ee
Purva Sharma <71826879+purvasharmadev@users.noreply.github.com>
2022-04-11 21:45:11 +0530

    feat: added onClick on quit button

--- src/Pages/Quiz/rules.jsx
+++ src/Pages/Quiz/rules.jsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
+import { useQuiz } from "../../Context/quiz-context";
 import { Link } from "react-router-dom";
 function Rules() {
   const [display, setDisplay] = useState("flex");
-
+  const {dispatch} = useQuiz()
   const modalDiv = { display: display };
 
   return (
@@ -14,19 +15,19 @@ function Rules() {
               <h2 className="color-primary text-lg">Rules</h2>
             </div>
             <div className="modal-text color-white p-1 text-md">
-              <ol class="list list-broder">
-                <li class="list-item list-item-border  list-item-background">
+              <ol className="list list-broder">
+                <li className="list-item list-item-border  list-item-background">
                   There are 10 questions , you have to select the right answer
                   amoung the given choices.
                 </li>
-                <li class="list-item list-item-border  list-item-background">
+                <li className="list-item list-item-border  list-item-background">
                   For each correct answer you will get +5 and for each wrong
                   answer -1.
                 </li>
-                <li class="list-item list-item-border  list-item-background">
+                <li className="list-item list-item-border  list-item-background">
                   Answer will be revealed after the last questions.
                 </li>
-                <li class="list-item list-item-border  list-item-background">
+                <li className="list-item list-item-border  list-item-background">
                   You are advised to not cheat while playing the quiz.
                 </li>
               </ol>
@@ -41,7 +42,7 @@ function Rules() {
                 Understood!
               </button>
               <button className="modal-btn">
-                <Link className="modal-btn" to="/">
+                <Link onClick={()=>dispatch({type:"clear_default"})} className="modal-btn" to="/">
                   Quit
                 </Link>
               </button>
