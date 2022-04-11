6052953f8ffbaf495a65069a5e99b30efc0fda34
Purva Sharma <71826879+purvasharmadev@users.noreply.github.com>
2022-04-11 21:47:18 +0530

    feat: added text for api errors

--- src/Pages/PageNotFound.jsx
+++ src/Pages/PageNotFound.jsx
@@ -1,11 +1,17 @@
+import {useQuiz} from "../Context/quiz-context"
 function PageNotFound() {
+  const {apiError} = useQuiz()
   return (
     <div
       style={{ height: "100vh" }}
       className="container flex flex-space-center align-item-center color-primary"
     >
       <div>
-        <h1 style={{ fontSize: "5rem" }}>404 - Page Not Found</h1>
+        <h1 style={{ fontSize: "5rem" }}>
+          {
+            apiError === true ? "Something went wrong! try again" : "404 - Page Not Found"
+          }
+          </h1>
       </div>
     </div>
   );
