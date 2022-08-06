3526eddf1cf9a543e9f5b461f4f175acad170699
Purva sharma <purva08815294379@gmail.com>
2022-08-06 11:00:38 +0530

    style: minor changes

--- src/Pages/PageNotFound.jsx
+++ src/Pages/PageNotFound.jsx
@@ -1,17 +1,17 @@
-import {useQuiz} from "../Context/quiz-context"
+import { useQuiz } from "../Context/quiz-context";
+import { Link } from "react-router-dom";
 function PageNotFound() {
-  const {apiError} = useQuiz()
+  const { apiError } = useQuiz();
   return (
     <div
       style={{ height: "100vh" }}
       className="container flex flex-space-center align-item-center color-primary"
     >
-      <div>
-        <h1 style={{ fontSize: "5rem" }}>
-          {
-            apiError === true ? "Something went wrong! try again" : "404 - Page Not Found"
-          }
-          </h1>
+      <div className="flex flex-justify-center align-item-center flex-column">
+        <h3>{apiError ? <>{apiError}</> : "404 - Page Not Found"}</h3>
+        <Link className="link color-primary" to="/">
+          Go to home
+        </Link>
       </div>
     </div>
   );
