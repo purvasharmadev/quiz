c3de7aaf440cfb537d62287786311d8ced69380f
Purva sharma <purva08815294379@gmail.com>
2022-08-09 07:20:26 +0530

    feat: added types for fetch function

--- src/Services/fetchQues.ts
+++ src/Services/fetchQues.ts
@@ -1,12 +1,12 @@
 import axios from "axios";
 
-export async function fetchQues(category) {
+export async function fetchQues(category:number) {
     try {
       const { data } = await axios.get(
         `https://opentdb.com/api.php?amount=10&category=${category}&type=multiple`
       )
       return data
-    } catch (error) {
+    } catch (error:any) {
      return error.Error
     }
   }
